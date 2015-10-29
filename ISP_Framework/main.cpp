72d6da6b07a44711f0d76715c12c747f4507af7b
FinnChou <zhoufan1148158886@yahoo.co.jp>
2015-10-30 00:25:12 +0800

    创建工程

---
+++ ISP_Framework/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ISP_Framework
+//
+//  Created by 周凡 on 15/10/30.
+//  Copyright © 2015年 FinnChou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
